5e6aba86f803cb7be248add8af8b76a7c3969cf3
truth-r <coder.rg7@gmail.com>
2020-08-02 16:44:18 +0530

    cleaning main.py source file

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+# importing libraries
 import os
+import pickle
 
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 
 from fastai.vision import load_learner, Path, open_image
 
-import pickle
-
+# creating a fastai path object for seamless navigation
 path = Path()
 
+# creating model object from pickle file
 model = load_learner(path, 'model/export.pkl')
 
+# creating flask app
 app = Flask(__name__)
 
-upload_folder = "static/"
+upload_folder = "static/" # user uploaded image will be saved here
 
 @app.route("/", methods=["GET", "POST"])
 
@@ -33,5 +34,6 @@ def upload_predict():
             return render_template("predict.html", output=prediction.obj)
     return render_template("index.html")
 
+# for running natively on user device
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
